10e11887654909ce2975dea84f2db1f6ca3f86a6
Aplet123 <jasonqan2004@gmail.com>
2017-12-14 16:54:20 -0500

    more testing

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var svg = d3.select("#main");
 svg.append("rect").attr("width", "100%").attr("height", "100%").attr("fill", "#0C032E");
-svg.append("rect").attr("width", "60%").attr("height", "20%").attr("fill", "#0C032E").attr("stroke-width", "5").attr("stroke", "#34034F").classed("top", true);
-svg.append("text").classed("title", true).html("TRIANGULUM").attr("width", "60%").attr("height", "20%").attr("text-anchor", "middle").attr("alignment-baseline", "central");
+svg.append("rect").attr("width", "60%").attr("height", "20%").attr("x", "20%").attr("fill", "#0C032E").attr("stroke-width", "5").attr("stroke", "#34034F").classed("top", true);
+svg.append("text").classed("title", true).html("TRIANGULUM").attr("width", "60%").attr("height", "20%").attr("x", "20%").attr("y", "0").attr("text-anchor", "middle").attr("alignment-baseline", "central");
 var vp = svg.append("g");
 for (var i = 0; i < 200; i ++) {
 	vp.append("circle").attr("cx", Math.floor(Math.random()*100) + "%").attr("cy", Math.floor(Math.random()*100) + "%").attr("r", Math.floor(Math.random()*10)).attr("fill", "url(#fadeoutwhite)");
