8488b93d4acddb47db82f05d24fd1a2749579238
Aplet123 <jasonqan2004@gmail.com>
2017-12-14 16:31:09 -0500

    more stars

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var svg = d3.select("#main");
 svg.append("rect").attr("width", "100%").attr("height", "100%").attr("fill", "#15044F");
 var vp = svg.append("g");
-vp.append("circle").attr("cx", "50").attr("cy", 50).attr("r", "20").attr("fill", "url(#fadeoutwhite)");
+for (var i = 0; i < 100; i ++) {
+	vp.append("circle").attr("cx", Math.floor(Math.random()*100) + "%").attr("cy", Math.floor(Math.random()*100) + "%").attr("r", Math.floor(Math.random()*10)).attr("fill", "url(#fadeoutwhite)");
+}
