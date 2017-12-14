c31e1a286085c3463c2be89b875fc18a016735da
Aplet123 <jasonqan2004@gmail.com>
2017-12-14 16:25:40 -0500

    star test

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 var svg = d3.select("#main");
 svg.append("rect").attr("width", "300%").attr("height", "300%").attr("fill", "#15044F").attr("x", "-100%").attr("y", "-100%");
+svg.append("circle").attr("cx", "50").attr("cy", 50).attr("r", "20").attr("fill", "url(#fadeoutwhite)");
