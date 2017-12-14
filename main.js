dbe378405fd496d12b0f33c0e47ac4b3fb71edca
Aplet123 <jasonqan2004@gmail.com>
2017-12-14 16:19:26 -0500

    oops

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
-var svg = document.getElementById("main");
+var svg = d3.select("#main");
 svg.append("rect").attr("width", "300%").attr("height", "300%").attr("fill", "#15044F").attr("x", "-100%").attr("y", "-100%");
