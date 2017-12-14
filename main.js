c5894eb0941405c706dad19a585a4c95a15a9718
Aplet123 <jasonqan2004@gmail.com>
2017-12-14 16:41:01 -0500

    text box

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var svg = d3.select("#main");
 svg.append("rect").attr("width", "100%").attr("height", "100%").attr("fill", "#0C032E");
+svg.append("rect").attr("width", "60%").attr("height", "20%").attr("fill", "#0C032E").attr("stroke-width", "5").attr("stroke", "#34034F");
 var vp = svg.append("g");
 for (var i = 0; i < 200; i ++) {
 	vp.append("circle").attr("cx", Math.floor(Math.random()*100) + "%").attr("cy", Math.floor(Math.random()*100) + "%").attr("r", Math.floor(Math.random()*10)).attr("fill", "url(#fadeoutwhite)");
