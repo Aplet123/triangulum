f3d6fc0e3826968451b3130174f7e81c412b003f
Aplet123 <jasonqan2004@gmail.com>
2017-12-14 16:27:35 -0500

    changes again

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 var svg = d3.select("#main");
-svg.append("rect").attr("width", "300%").attr("height", "300%").attr("fill", "#15044F").attr("x", "-100%").attr("y", "-100%");
-svg.append("circle").attr("cx", "50").attr("cy", 50).attr("r", "20").attr("fill", "url(#fadeoutwhite)");
+svg.append("rect").attr("width", "100%").attr("height", "100%").attr("fill", "#15044F");
+var vp = svg.append("g");
+vp.append("circle").attr("cx", "50").attr("cy", 50).attr("r", "20").attr("fill", "url(#fadeoutwhite)");
